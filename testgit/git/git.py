8bb1decdba91e64302540e3084a9f9cbf697a695
fuzicheng <fuzicheng@noahgroup.com>
2020-12-08 14:58:35 +0800

    1

--- testgit/git/git.py
+++ testgit/git/git.py
@@ -1,27 +1,5 @@
-import requests
+import datetime
 
+now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-class Demo:
-    addUser_url = 'https://zhizi-release.arkfintech.cn/'
-
-    def test(self, url):
-        new_url = self.addUser_url + url
-        userData = {
-            "jsCode": "013FJR4l1hBuzq0ziN4l18KR4l1FJR4g"
-        }
-        rep = requests.post(new_url, data=userData)
-        print(rep.text)
-        getaddUser = (rep.json()['sessionKey'])
-        print(getaddUser)
-
-        if getaddUser is None:
-
-            print("失败")
-        else:
-
-            print("成功")
-
-
-d = Demo()
-
-d.test('weixin/getSessionKey')
+print(now_time)
