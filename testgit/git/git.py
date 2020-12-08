1e14535c49380c4d3f9c0fc4a8c94a490d3443b1
fuzicheng <fuzicheng@noahgroup.com>
2020-12-08 11:07:48 +0800

    1

---
+++ testgit/git/git.py
@@ -0,0 +1,20 @@
+import random
+
+
+def Unicode():
+    val = random.randint(0x4e00, 0x9fbf)
+    return chr(val)
+
+
+def GBK2312():
+    head = random.randint(0xb0, 0xf7)
+    body = random.randint(0xa1, 0xfe)
+    val = f'{head:x} {body:x}'
+    str = bytes.fromhex(val).decode('gb2312')
+    return str
+
+
+if __name__ == '__main__':
+    print("付")
+    print(Unicode())
+    print(GBK2312())
